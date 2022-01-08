bf459406878a26a1efae8542105cc40ad0f10ef4
Lynn Bradshaw <readyready15728@gmail.com>
2022-01-08 16:52:26 -0500

    Improving output quality

--- solver.py
+++ solver.py
@@ -7,11 +7,15 @@ with open(dictionary) as f:
     words = set(word.rstrip() for word in f.readlines())
 
 letters = sys.argv[1]
-possibilities = []
+possibilities = set()
 
 for i in range(2, len(letters) + 1):
     for p in itertools.permutations(letters, i):
-        possibilities.append(''.join(p))
+        possibilities.add(''.join(p))
+
+possibilities = list(possibilities)
+possibilities.sort()
+possibilities.sort(key=len)
 
 for p in possibilities:
     if p in words:
